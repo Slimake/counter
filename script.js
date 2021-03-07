2c6537a10ea94d0e8a6e48ce942b8a5a2da6b848
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-07 23:02:41 +0100

    Modify initial file

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const add = document.querySelector(".add");
 let counter = 0;
 
 function addCount() {
-	count.textContent = ++counter;
+	if (counter >= 0) {
+		count.textContent = ++counter;
+	}
 }
 
 function lowerCount() {
-	count.textContent = --counter;
+	if (counter > 0) {
+		count.textContent = --counter;
+	}
 }
 
 add.addEventListener("click", addCount);
 lower.addEventListener("click", lowerCount);
-
-
